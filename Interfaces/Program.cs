3bcd2b605d8678674241a88b682ef0287eb7abc5
merve <merve@MERVEA>
2024-02-18 16:24:48 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //Demo();
+
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleServerCustomerDal(),
+                new MySqlServerCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Merve",
+                LastName = "Altınışık",
+                Address = "Abc"
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Henry",
+                LastName = "Altınışık",
+                Departmant = "Software engineer"
+            };
+
+            Worker worker = new Worker
+            {
+                Id = 1,
+                FirstName = "Tom",
+                LastName = "Altınışık",
+                Departmant = "Software engineer"
+            };
+
+
+
+            manager.Add(customer);
+            manager.Add(student);
+            manager.Add(worker);
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+
+}
